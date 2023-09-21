e94d8259ca9414ed0fa15675c5c7fc53f2b063f2
FrhCode <farhan7534031b@gmail.com>
2023-09-21 11:23:26 +0700

    progress

--- src/main/java/com/farhan/skripsibe/config/SecurityConfiguration.java
+++ src/main/java/com/farhan/skripsibe/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.farhan.skripsibe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,9 +23,10 @@ public class SecurityConfiguration {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(crsf -> crsf.disable())
 				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 						.requestMatchers("/api/v1/auth/**").permitAll()
-						.requestMatchers("GET", "/api/v1/symptoms/all").permitAll()
-						.requestMatchers("GET", "/api/v1/consultations/*").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/v1/symptoms/all").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/v1/consultations/*").permitAll()
 						.anyRequest().authenticated())
 				.sessionManagement(session -> session
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
