8306047f1ac1aec70f042de5fef37d89c4f92f65
FrhCode <farhan7534031b@gmail.com>
2023-09-21 09:01:04 +0700

    progress

--- src/main/java/com/farhan/skripsibe/config/CorsConfig.java
+++ src/main/java/com/farhan/skripsibe/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
 		config.addAllowedOrigin("http://localhost:3000");
+		config.addAllowedOrigin("http://localhost:5173");
 		config.addAllowedOrigin("https://pakar.farhandev.cloud");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
