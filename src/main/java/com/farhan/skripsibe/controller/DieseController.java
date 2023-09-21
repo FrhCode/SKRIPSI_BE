477f5bd9c02c1854ea1299f02aebc716dcffebfd
FrhCode <farhan7534031b@gmail.com>
2023-09-21 08:26:10 +0700

    progress

--- src/main/java/com/farhan/skripsibe/controller/DieseController.java
+++ src/main/java/com/farhan/skripsibe/controller/DieseController.java
@@ -1,14 +1,17 @@
 package com.farhan.skripsibe.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.farhan.skripsibe.model.Diese;
+import com.farhan.skripsibe.model.Symptom;
 import com.farhan.skripsibe.request.PaginateDieseRequest;
 import com.farhan.skripsibe.service.DieseService;
 
@@ -34,4 +37,11 @@ public class DieseController {
 		return dieseService.paginate(paginateDieseRequest);
 	}
 
+	@GetMapping("{code}/symptoms")
+	public Map<String, Object> getSymtoms(@PathVariable String code) {
+		List<Symptom> symptoms = dieseService.getSymtoms(code);
+		Map<String, Object> response = new HashMap<>();
+		response.put("data", symptoms);
+		return response;
+	}
 }
--- src/main/java/com/farhan/skripsibe/service/DieseService.java
+++ src/main/java/com/farhan/skripsibe/service/DieseService.java
@@ -1,9 +1,12 @@
 package com.farhan.skripsibe.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import com.farhan.skripsibe.model.Diese;
+import com.farhan.skripsibe.model.Symptom;
 import com.farhan.skripsibe.repository.DieseCriteriaRepository;
 import com.farhan.skripsibe.repository.DieseRepository;
 import com.farhan.skripsibe.request.PaginateDieseRequest;
@@ -22,6 +25,10 @@ public class DieseService {
 
 	public Page<Diese> paginate(PaginateDieseRequest paginateDieseRequest) {
 		return dieseCriteriaRepository.paginate(paginateDieseRequest);
+	}
+
+	public List<Symptom> getSymtoms(String code) {
+		return dieseRepository.findByCode(code).get().getSymptoms();
 	}
 
 }
