7171a815a6641981e47a2347b8af6975a79ebc95
FrhCode <farhan7534031b@gmail.com>
2023-10-17 12:56:55 +0700

    progress

--- src/main/java/com/farhan/skripsibe/controller/SolutionController.java
+++ src/main/java/com/farhan/skripsibe/controller/SolutionController.java
@@ -7,12 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.farhan.skripsibe.model.Solution;
 import com.farhan.skripsibe.repository.SolutionRepository;
+import com.farhan.skripsibe.request.EditSolutionsRequest;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -27,6 +31,22 @@ public class SolutionController {
 		solutionRepository.delete(solution);
 		Map<String, String> response = new HashMap<>();
 		response.put("status", "deleted");
+
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@PutMapping("{id}")
+	public ResponseEntity<Object> editSolution(@PathVariable Long id,
+			@Valid @RequestBody EditSolutionsRequest editSolutionsRequest) {
+
+		Solution solution = solutionRepository.findById(id).get();
+		solution.setName(editSolutionsRequest.getName());
+		solution.setDescription(editSolutionsRequest.getDescription());
+
+		solutionRepository.save(solution);
+
+		Map<String, String> response = new HashMap<>();
+		response.put("status", "Success");
 
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
---
+++ src/main/java/com/farhan/skripsibe/request/EditSolutionsRequest.java
@@ -0,0 +1,17 @@
+package com.farhan.skripsibe.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EditSolutionsRequest {
+	@Size(min = 3, message = "Nama setidaknya terdiri dari 3 karakter")
+	private String name;
+
+	@Size(min = 3, message = "Deskripsi setidaknya terdiri dari 3 karakter")
+	private String description;
+}
